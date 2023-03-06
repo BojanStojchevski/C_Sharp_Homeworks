1f78d3eafdad1a36f54d3c6e09b670604c24333d
BojanStojchevski <108979808+BojanStojchevski@users.noreply.github.com>
2023-03-06 01:30:02 +0100

    Add files via upload

---
+++ Homework2/Homework2.StudentGroup/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string[] studentsG1 = new string[]{ "Bojan", "Goran", "Stojan","Zoran","Vojdan"};
+
+string[] studentsG2 = new string[] { "Ace", "Teo", "Leo", "Neo", "Jo" };
+
+
+Console.WriteLine("Write number 1 or 2..");
+
+int num; ;
+num = Int32.Parse(Console.ReadLine());
+
+    if (num == 1)
+    {
+    foreach (string g1 in studentsG1)
+    {
+    Console.WriteLine(g1);
+    }
+    }
+    else if(num == 2)
+    {
+        foreach (string g2 in studentsG2)
+    {
+        Console.WriteLine(g2);
+    }
+}
+else
+{
+    Console.WriteLine("You typed incorect number!");
+
+}
+
+
+
+    
+    
+
+    
+
+
+
+
+//for (int i = 0; i < studentsG1.Length; i++)
+//{
+//    g1 += studentsG1[i];
+//    //Console.WriteLine(g1);
+//}
+//int num = 1;
+//num = int.Parse(Console.ReadLine(g1));
+
+
+//studentsG2 = int.Parse(Console.ReadLine());
+
---
+++ Homework2/Homework2.SumOfEven/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+int sumOfEven = 0;
+
+int[] integers = new int[6];
+
+Console.WriteLine("Enter integer no.1:");
+
+integers[0] = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter integer no.2:");
+
+integers[1] = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter integer no.3:");
+integers[2] = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter integer no.4:");
+integers[3] = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter integer no.5:");
+integers[4] = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter integer no.6:");
+integers[5] = int.Parse(Console.ReadLine());
+
+
+foreach (int num  in  integers)
+{
+    if(num % 2  == 0)
+    {
+        sumOfEven += num;
+    }
+  Console.WriteLine(" The result is: " + sumOfEven);
+}
+
+
+
+
